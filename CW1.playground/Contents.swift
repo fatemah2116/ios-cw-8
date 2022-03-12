54364423753da7e0aadbe83d2ad6a715e89e2da7
fatamah <fatiabbasali2018@gmail.com>
2022-03-12 12:53:58 +0300

    fati88

--- CW1.playground/Contents.swift
+++ CW1.playground/Contents.swift
@@ -2,4 +2,18 @@ import UIKit
 
 // التمرين الأول
 // ClassWork 1
+var name = "fatamah zahra"
 
+var birthdayyear = 2006
+
+var currentyear = 2022
+
+var age = currentyear-birthdayyear
+ print("hello my name is \(name) and my age is \(age)")
+
+var number1 = "8"
+var number2 = 4
+var number3 = 2.5
+
+number3 = (Double (number1) ?? 0) + Double(number2)
+ 
